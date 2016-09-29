44804f3781ddfbfffd13f694b32891546ab508a7
otequio <cesar.arovai@gmail.com>
2016-09-29 20:42:44 -0300

    Add database file

---
+++ config/database.sql
@@ -0,0 +1,32 @@
+-- Database - FilaFacil
+create database filafacil;
+
+use filafacil;
+
+create table funcionario (
+	codfuncionario int auto_increment primary key,
+	nome varchar(255) not null,
+	status tinyint(1) not null default 0,
+	email varchar(100) not null,
+	senha varchar(20) not null,
+	descricao varchar(255),
+	disponivel tinyint(1) not null default 0,
+	dtcadastro datetime not null,
+	tipo tinyint(1) not null default 0 -- 0 secretária / 1 atendente
+);
+
+create table fila (
+	codfila int auto_increment primary key,
+	ra varchar(20) not null,
+	status tinyint(1) not null default 1
+);
+
+create table atendimentos (
+	codatendimento int auto_increment primary key,
+	codfuncionario int not null,
+	codfila int not null,
+	dtinicio datetime,
+	dtfim datetime,
+	foreign key (codfuncionario) references funcionario(codfuncionario),
+	foreign key (codfila) references fila(codfila)
+);
